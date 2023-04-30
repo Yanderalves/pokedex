1c3d5a88dd9703fbd5f519ca4100dbd81b0c3a9d
Yander <yanderceara@gmail.com>
2023-04-30 10:34:05 -0300

    Melhoria na estilização da listagem de pokemons

--- src/App.js
+++ src/App.js
@@ -8,37 +8,43 @@ export default function App() {
   const [endpoints, setEndpoints] = useState([]);
   const [pokemons, setPokemons] = useState([]);
 
-  useEffect(() => {
-    api
-      .get("/pokemon/")
-      .then((response) => setEndpoints(response.data.results))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
+  let offset = 0;
 
-  useEffect(() => {
-    const pokemonRequests = endpoints.map(endpoint =>
-      axios.get(endpoint.url)
-    );
-    Promise.all(pokemonRequests)
-      .then(responses => {
-        const pokemonData = responses.map(response => response.data);
-        setPokemons(pokemonData);
-      })
-      .catch(error => {
-        console.log(`Erro na requisição: ${error}`);
-      });
-  }, [endpoints]);
+  const getAllPokemons = () =>{
+    useEffect(() => {
+      api
+        .get(`/pokemon?limit=20&offset=${offset}`)
+        .then((response) => setEndpoints(response.data.results))
+        .catch((err) => {
+          console.error("ops! ocorreu um erro" + err);
+        });
+    }, []);
 
+    useEffect(() => {
+      const pokemonRequests = endpoints.map(endpoint =>
+        axios.get(endpoint.url)
+      );
+      Promise.all(pokemonRequests)
+        .then(responses => {
+          const pokemonData = responses.map(response => response.data);
+          setPokemons(pokemonData);
+        })
+        .catch(error => {
+          console.log(`Erro na requisição: ${error}`);
+        });
+    }, [endpoints]);
+  }
+  getAllPokemons()
   return (
     <section className="content">
       <h1>Pokedex</h1>
       <ul className="pokemons">
         {pokemons.map(pokemon => (
-          <Pokemon id={pokemon.id} image={pokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']} key={pokemon.id} name={pokemon.name} types={pokemon.types} />
+          <Pokemon id={pokemon.id} image={pokemon.sprites.other['official-artwork'].front_default ||
+          pokemon.sprites.front_default} key={pokemon.id} name={pokemon.name} types={pokemon.types} />
         ))}
       </ul>
+      {/* <button onClick={}>More Pokemons</button> */}
     </section>
   );
 }
--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 
+const path = '/assets/types/';
+
 export default function Pokemon(props){
     const typePrimary = props.types[0].type.name;
     return(
         <li className={`pokemon colour-${typePrimary}`}>
-            <span className="number">{props.id}</span>
+            <span className="number"># {props.id}</span>
             <span className="name">{props.name}</span>
             <div className="details">
                 <ul className="types">
-                    {props.types.map(type => <li className="type">{type.type.name}</li>)}
+                    {props.types.map(type =>
+                         <li className={`type colour-${type.type.name}`}>
+                            <img src={`${path}${type.type.name}.svg`}></img>
+                            {type.type.name}
+                         </li>)}
                 </ul>
                 <img className="image"
                     src={props.image}
