d4c975e60d383fd22c313857f527c79ca145e51b
Yander <yanderceara@gmail.com>
2023-05-09 23:07:39 -0300

    Melhoria na componetização

--- src/App.js
+++ src/App.js
@@ -1,77 +1,9 @@
-import React, { useEffect, useState } from "react";
-import api from "./services/api";
-import Pokemon from "./components/Pokemon";
-import axios from "axios";
-import './App.css'
-import "./colors.css";
-import Loading from "./components/Loading";
-
-const TOTAL_POKEMON_API = 1281;
-
+import React from 'react';
+import Home from "./Pages/Home";
 
 export default function App() {
-  const [endpoints, setEndpoints] = useState([]);
-  const [pokemons, setPokemons] = useState([]);
-  const [search, setSearch] = useState("");
-  const [pokemonsFiltered, setPokemonsFiltered] = useState([]);
-  const [displayCount, setDisplayCount] = useState(9);
-
-  useEffect(() => {
-    api
-      .get(`/pokemon?limit=${displayCount}&offset=0`)
-      .then((response) => setEndpoints(response.data.results))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, [displayCount]);
-
-  useEffect(() => {
-    const pokemonRequests = endpoints.map(endpoint =>
-      axios.get(endpoint.url)
-    );
-    Promise.all(pokemonRequests)
-      .then(responses => {
-        const pokemonData = responses.map(response => response.data);
-        setPokemons(pokemonData);
-      })
-      .catch(error => {
-        console.log(`Erro na requisição: ${error}`);
-      });
-  }, [endpoints]);
-
-  useEffect(() => {
-    setPokemonsFiltered(pokemons.filter(pokemon => pokemon.name.includes(search)));
-  }, [search, pokemons])
-
-  const mapperPokemon = (pokemon) => {
-    return {
-      name: pokemon.name,
-      id: pokemon.id,
-      image: pokemon['sprites']['other']['official-artwork']['front_default'] || pokemon.sprites.other.dream_world.front_default,
-      types: pokemon.types,
-    }
-  }
 
   return (
-    <section className="content">
-      <div className="head-page">
-        <img alt="pokedex-title" className="page-title" src="./assets/patterns/pokedex-title.png"></img>
-        <div className="search">
-          <input type="text" placeholder="FAAAAAAAAAAAAAAALA ZEZE" onChange={(e => setSearch(e.target.value.toLowerCase().trim()))} />
-          <button className="button-search">
-          </button>
-        </div>
-      </div>
-      {pokemonsFiltered && pokemonsFiltered.length > 0 ? <ul className="pokemons"  >
-        {pokemonsFiltered.slice(0, displayCount).map((pokemon) => (
-          < Pokemon key={pokemon.id} pokemon={mapperPokemon(pokemon)} />
-        ))}
-      </ul> :
-        <Loading />}
-
-      {((displayCount < TOTAL_POKEMON_API) && pokemonsFiltered.length > 0) &&
-        <button className="button-more" onClick={(e) => setDisplayCount(displayCount + 21)}>Mostrar mais</button>
-      }
-    </section>
+    <Home />
   );
 }
